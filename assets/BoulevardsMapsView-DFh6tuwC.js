ba95b5824d8be7bf12c1f0971bb236247d176a23
dbalinov <dbalinov@users.noreply.github.com>
2025-04-13 17:55:28 +0000

    deploy: 5fd85368edfe0d5afaf7c8a7a78a07aa42cd0992

--- assets/BoulevardsMapsView-DFh6tuwC.js
+++ assets/BoulevardsMapsView-DFh6tuwC.js
@@ -1,4 +1,4 @@
-import{W as u,a as p,V as w,_ as b,b as g}from"./index-EGzlvahn.js";import{d as h,r as C,c as A,o as f,a as d,b as O,e as k,w as v,F as N,f as P,g as l,h as E,_ as I}from"./index-8jTyUR94.js";const T=`
+import{W as u,a as p,V as w,_ as b,b as h}from"./index-D7qWMBhM.js";import{d as g,r as C,c as A,D as O,E as d,F as f,j as k,f as v,G as E,H as N,o,e as P}from"./vuetify-Bz3L5cxd.js";import"./leaflet-B44856_d.js";import"./index-CmzqPN2c.js";const T={class:"map-container"},I=`
 SELECT ?id ?namesake ?namesake_desc
   (SAMPLE(?img) AS ?IMG)
   ?title
@@ -10,7 +10,6 @@ SELECT ?id ?namesake ?namesake_desc
     CONCAT('<hr>[', str(?namesake), ' ', ?namesake_label, ']')
   ) AS ?description)
   (CONCAT('#', substr(SHA1(CONCAT(str(?id), 'kur')), 1, 6)) AS ?stroke)
-  (4 AS ?stroke_width)
   (?osm AS ?osmRelationId)
 WHERE {
   ?id wdt:P31 wd:Q54114 ;
@@ -40,7 +39,7 @@ WHERE {
   }
   BIND (COALESCE(CONCAT('<a href="https://bg.wikipedia.org/wiki/', ?namesake_wiki_name, '" target="_blank">', ?namesake_wiki_name, '</a>'), ?namesake_label) AS ?title)
 }
-GROUP BY ?id ?title ?namesake ?namesake_label ?namesake_desc ?namesake_wiki_name ?namesake_wiki_link ?osm`,S=h({__name:"BoulevardsMapsView",setup(B){const m=u({instance:"https://www.wikidata.org",sparqlEndpoint:"https://query.wikidata.org/sparql"}).sparqlQuery(T),n=C(),c=A(()=>{var s;return((s=n.value)==null?void 0:s.results.bindings.map(e=>{var a;return(a=e.osmRelationId)==null?void 0:a.value}))||[]});function _(s){var a,t;const e=(((a=n.value)==null?void 0:a.results.bindings)||[]).find(i=>{var r;return((r=i.osmRelationId)==null?void 0:r.value)==s});return`
+GROUP BY ?id ?title ?namesake ?namesake_label ?namesake_desc ?namesake_wiki_name ?namesake_wiki_link ?osm`,V=g({__name:"BoulevardsMapsView",setup(S){const m=u({instance:"https://www.wikidata.org",sparqlEndpoint:"https://query.wikidata.org/sparql"}).sparqlQuery(I),n=C(),c=A(()=>{var s;return((s=n.value)==null?void 0:s.results.bindings.map(e=>{var a;return(a=e.osmRelationId)==null?void 0:a.value}))||[]});function _(s){var a,t;const e=(((a=n.value)==null?void 0:a.results.bindings)||[]).find(i=>{var r;return((r=i.osmRelationId)==null?void 0:r.value)==s});return`
     <div align="center">
       ${e==null?void 0:e.title.value}<br>`+(e!=null&&e.IMG?`<img src="${(t=e==null?void 0:e.IMG)==null?void 0:t.value}" alt="Image" style="width: 100px; height: auto;"><br>`:"")+`${e==null?void 0:e.description.value}<br>
-    </div>`}return f(async()=>{try{const s=await p.get(m);n.value=s.data}catch(s){console.error("Error fetching data:",s)}}),(s,e)=>(l(),d("div",null,[e[0]||(e[0]=O("h1",null,"Булеварди в София и тяхната памет",-1)),k(w,{center:[42.698334,23.319941]},{default:v(()=>[k(b),(l(!0),d(N,null,P(c.value,a=>{var t,i;return l(),E(g,{key:a,id:a,stroke:(i=(t=n.value)==null?void 0:t.results.bindings.find(r=>{var o;return((o=r.osmRelationId)==null?void 0:o.value)==a}))==null?void 0:i.stroke.value,"stroke-width":5,"popup-content":_(a)},null,8,["id","stroke","popup-content"])}),128))]),_:1})]))}}),V=I(S,[["__scopeId","data-v-aae3d469"]]);export{V as default};
+    </div>`}return O(async()=>{try{const s=await p.get(m);n.value=s.data}catch(s){console.error("Error fetching data:",s)}}),(s,e)=>(o(),d("div",T,[e[0]||(e[0]=f("h1",{class:"text-h4 bg-white pa-3"},"Булеварди в София и тяхната памет",-1)),k(w,{center:[42.698334,23.319941]},{default:v(()=>[k(b),(o(!0),d(E,null,N(c.value,a=>{var t,i;return o(),P(h,{key:a,id:a,stroke:(i=(t=n.value)==null?void 0:t.results.bindings.find(r=>{var l;return((l=r.osmRelationId)==null?void 0:l.value)==a}))==null?void 0:i.stroke.value,"stroke-width":4,"popup-content":_(a)},null,8,["id","stroke","popup-content"])}),128))]),_:1})]))}});export{V as default};
--- assets/MapsView-DWFIEAcf.js
+++ assets/MapsView-DWFIEAcf.js
@@ -1,10 +1,8 @@
-import{W as p,a as c,V as u,_ as b,b as g}from"./index-EGzlvahn.js";import{d as h,r as v,c as A,o as f,a as k,b as C,e as o,w as P,F as O,f as S,g as r,h as T,_ as x}from"./index-8jTyUR94.js";const E=`
+import{W as p,a as c,V as u,_ as b,b as g}from"./index-D7qWMBhM.js";import{d as h,r as v,c as A,D as f,E as o,F as C,j as k,f as P,o as r,G as O,H as E,e as T}from"./vuetify-Bz3L5cxd.js";import"./leaflet-B44856_d.js";import"./index-CmzqPN2c.js";const x={class:"map-container"},N=`
 SELECT ?id ?namesake (SAMPLE(?img) AS ?IMG) (SAMPLE(?kvartal_wikitext) AS ?KVARTAL_WT)
   (CONCAT('<big>', ?namesake_wikitext, '</big>') AS ?title)
-  (COALESCE(CONCAT('<hr>ул. <em><a href="https://www.openstreetmap.org/relation/', ?osm, '" target="_blank">', ?bul_label, '</a></em> в кв.', 
-  ?KVARTAL_WT, ', носи името на растението ', ?namesake_wikitext, ' (<em><a href="https://www.ipni.org/n/', ?ipni, '" target="_blank">', ?taxon_name, '</a></em>)'), CONCAT('<hr>[', str(?id), ' обект в уикиданни]')) AS ?description)
+  (COALESCE(CONCAT('<hr>ул. <em><a href="https://www.openstreetmap.org/relation/', ?osm, '" target="_blank">', ?bul_label, '</a></em> в кв.', ?KVARTAL_WT, ', носи името на растението ', ?namesake_wikitext, ' (<em><a href="https://www.ipni.org/n/', ?ipni, '" target="_blank">', ?taxon_name, '</a></em>)'), CONCAT('<hr>[', str(?id), ' обект в уикиданни]')) AS ?description)
   (CONCAT('#', substr(SHA1(CONCAT(str(?id), 'cvete')), 1, 6)) AS ?stroke)
-  (5 AS ?stroke_width)
   (?osm AS ?osmRelationId)
 WHERE {
   ?id wdt:P31 wd:Q79007 ;
@@ -44,9 +42,9 @@ WHERE {
   BIND (CONCAT('<a href="https://bg.wikipedia.org/wiki/', ?kvartal_wiki_name, '" target="_blank">', ?kvartal_wiki_name, '</a>') AS ?kvartal_wikitext)
 }
 GROUP BY ?id ?title ?namesake ?namesake_label ?namesake_desc ?namesake_wiki_name ?namesake_wiki_link
-  ?taxon_name ?ipni ?bul_label ?osm ?namesake_wikitext`,N=h({__name:"MapsView",setup(I){const d=p({instance:"https://www.wikidata.org",sparqlEndpoint:"https://query.wikidata.org/sparql"}).sparqlQuery(E),s=v(),m=A(()=>{var t;return((t=s.value)==null?void 0:t.results.bindings.map(a=>{var e;return(e=a.osmRelationId)==null?void 0:e.value}))||[]});function _(t){var e;const a=(((e=s.value)==null?void 0:e.results.bindings)||[]).find(n=>{var i;return((i=n.osmRelationId)==null?void 0:i.value)==t});return`
+  ?taxon_name ?ipni ?bul_label ?osm ?namesake_wikitext`,y=h({__name:"MapsView",setup(S){const m=p({instance:"https://www.wikidata.org",sparqlEndpoint:"https://query.wikidata.org/sparql"}).sparqlQuery(N),s=v(),d=A(()=>{var t;return((t=s.value)==null?void 0:t.results.bindings.map(a=>{var e;return(e=a.osmRelationId)==null?void 0:e.value}))||[]});function w(t){var e;const a=(((e=s.value)==null?void 0:e.results.bindings)||[]).find(n=>{var i;return((i=n.osmRelationId)==null?void 0:i.value)==t});return`
     <div align="center">
       ${a==null?void 0:a.title.value}<br>
       <img src="${a==null?void 0:a.IMG.value}" alt="Image" style="width: 100px; height: auto;"><br>
       ${a==null?void 0:a.description.value}<br>
-    </div>`}return f(async()=>{try{const t=await c.get(d);s.value=t.data}catch(t){console.error("Error fetching data:",t)}}),(t,a)=>(r(),k("div",null,[a[0]||(a[0]=C("h1",null,"Улици в София с име на растение",-1)),o(u,{center:[42.698334,23.319941]},{default:P(()=>[o(b),(r(!0),k(O,null,S(m.value,e=>{var n,i;return r(),T(g,{key:e,id:e,stroke:(i=(n=s.value)==null?void 0:n.results.bindings.find(w=>{var l;return((l=w.osmRelationId)==null?void 0:l.value)==e}))==null?void 0:i.stroke.value,"stroke-width":5,"popup-content":_(e)},null,8,["id","stroke","popup-content"])}),128))]),_:1})]))}}),M=x(N,[["__scopeId","data-v-459566c3"]]);export{M as default};
+    </div>`}return f(async()=>{try{const t=await c.get(m);s.value=t.data}catch(t){console.error("Error fetching data:",t)}}),(t,a)=>(r(),o("div",x,[a[0]||(a[0]=C("h1",{class:"text-h4 bg-white pa-3"},"Улици в София с име на растение",-1)),k(u,{center:[42.698334,23.319941]},{default:P(()=>[k(b),(r(!0),o(O,null,E(d.value,e=>{var n,i;return r(),T(g,{key:e,id:e,stroke:(i=(n=s.value)==null?void 0:n.results.bindings.find(_=>{var l;return((l=_.osmRelationId)==null?void 0:l.value)==e}))==null?void 0:i.stroke.value,"stroke-width":5,"popup-content":w(e)},null,8,["id","stroke","popup-content"])}),128))]),_:1})]))}});export{y as default};
